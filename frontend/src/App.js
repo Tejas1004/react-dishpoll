2b7ec4252e1a36afd4cb93eef70a406fa225901f
Your Name <tejaszanzane2001@gmail.com>
2022-02-01 21:37:52 +0530

    login

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,22 +1,23 @@
 import React from 'react';
 import { BrowserRouter as  Router, Route, Routes }from 'react-router-dom';
-import Login from './Components/Login';
+import Login from './Components/login';
 import Form from './Components/Polling/form';
+import Start from "./Components/Polling/start";
+import Result from './Components/Polling/result';
 
 const  App = () => {
-  return( 
-  <>
-  <Router >
-      <Routes>
-      <Route path="/" element={<Login/>} exact />
-      <Route path="/form" element={<Form/>}  />
-  
-      
-      </Routes>
-    </Router>
-  
-  </>
-  );
-};
+ 
+return (
+  <Router>
+    <div className="conatiner">
+      <Routes> <Route path="/" element={<Login />} /></Routes>
+      <Routes> <Route path="/form" element={<Form />} /></Routes>
+      <Routes><Route path="/start" element={<Start />} /></Routes>
+      <Routes><Route path="/result" element={<Result />} /></Routes>
+    </div>
+  </Router>
+);
+}
 
 export default  App;
+
--- frontend/src/Components/Polling/form.js
+++ frontend/src/Components/Polling/form.js
@@ -1,13 +1,13 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
-export default function Dashboard() {
+export default function Form() {
   const navigate = useNavigate();
   return (
-    <div className="board">
-      <div className="card" onClick={() => { navigate("/start");}} >
+    <div className="conatiner">
+      <div className="poll" onClick={() => { navigate("/start");}} >
         Start Polling
       </div>
-      <div  className="card"onClick={() => { navigate("/Result");}}>
+      <div  className="poll"onClick={() => { navigate("/result");}}>
         View Polling
       </div>
     </div>
